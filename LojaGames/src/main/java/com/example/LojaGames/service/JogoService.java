bf6bd56415d3fb905186d224779ba95555c994d0
Pingu <felipe_guaresi@estudante.sesisenai.org.br>
2026-04-07 22:09:29 -0300

    Add files via upload

---
+++ LojaGames/src/main/java/com/example/LojaGames/service/JogoService.java
@@ -0,0 +1,37 @@
+package com.example.LojaGames.service;
+
+import com.example.LojaGames.model.Jogo;
+import com.example.LojaGames.repository.JogoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JogoService {
+
+    private final JogoRepository repository;
+
+    public JogoService(JogoRepository repository){
+        this.repository = repository;
+    }
+    public Jogo salvar(Jogo jogo){
+        return repository.save(jogo);
+    }
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
+    public List<Jogo> listar(){
+        return repository.findAll();
+    }
+    public Jogo atulizar(Long id, Jogo jogo){
+        buscarPorId(id);
+        jogo.setId(id);
+        return repository.save(jogo);
+    }
+    public Jogo buscarPorId(Long id){
+        return repository.findById(id).orElseThrow(() -> new RuntimeException(" Jogo n encontrado"));
+    }
+    public List<Jogo> buscarPorNome(String nome){
+        return repository.findBynomeContainingIgnoreCase(nome);
+    }
+}
